d180fb0d933fa8db77bfff8fdc023977662a5fd2
Nathan Friedly <nathan@nfriedly.com>
2015-10-14 07:01:03 -0400

    fixing typo and adding gamecrate sweepstakes

--- newegg-bot.casper.js
+++ newegg-bot.casper.js
@@ -3,10 +3,12 @@
 "use strict";
 
 var today = new Date();
+var delay;
 var returnCode = 0;
 
 function handleTimeout() {
     /*jshint validthis:true */
+    this.debugHTML();
     this.capture('./timeout.png');
     this.echo("Timeout reached, screenshot saved");
     returnCode = 1;
@@ -48,6 +50,7 @@ casper.thenOpen('https://m.facebook.com/Newegg', function() {
 });
 
 
+
 if (today < new Date('October 19, 2015 11:59 PM PST')) {
     casper.then(function () {
         this.echo('beginning game like a pro sweepstakes');
@@ -57,7 +60,48 @@ if (today < new Date('October 19, 2015 11:59 PM PST')) {
         //this.debugHTML();
     });
     if (slow) {
-        var delay = (new Date().getDate()-10)*2 + Math.random();
+        casper.then(function() {
+            delay = (new Date().getDate()-10)*2 + Math.random();
+            this.echo('waiting ' + delay + ' minutes');
+        });
+        casper.wait((delay*60)*1000);
+    }
+    casper.withFrame(0, function() {
+        //this.capture('./screen.png');
+        //this.debugHTML();
+        this.fill('#form_form', {
+            'form[email]': 'nathan.friedly+newegg@gmail.com',
+            'form[first_name]': 'Nathan',
+            'form[last_name]': 'Friedly',
+            'form[address_1]': '7527 Cox Rd',
+            'form[city]': 'Pleasant Hill',
+            'form[province]': 'OH',
+            'form[postal_code]': '45359',
+            'form[phone]': '937-409-1337',
+            'form[age]': '29'
+        }, true);
+
+        casper.waitForText("Thank you for your submission", function() {
+            this.capture('./gamelikeapro.png');
+            this.echo("Done with game like a pro sweepstakes!");
+        });
+    });
+} else {
+    casper.then(function () {
+        this.echo('skipping game like a pro sweepstakes');
+    });
+}
+
+if (today < new Date('November 13, 2015 11:59 PM PST')) {
+    casper.then(function () {
+        this.echo('beginning gamecrate lucky 13 corsair giveaway');
+    });
+    casper.thenOpen('http://www.gamecrate.com/lucky-13-corsair-giveaway/11352?cm_mmc=SNC-Facebook-_-NA-_-GameCrate-lucky13corsair-_-NA', function login() {
+        //this.capture('./screen.png');
+        //this.debugHTML();
+    });
+    if (slow) {
+        delay = (new Date().getDate()-10)*2 + Math.random();
         casper.then(function() {
             this.echo('waiting ' + delay + ' minutes');
         });
@@ -77,18 +121,17 @@ if (today < new Date('October 19, 2015 11:59 PM PST')) {
             'form[phone]': '937-409-1337',
             'form[age]': '29'
         }, true);
-    });
-    casper.waitForText("Thank you for yoyr submission", function() {
-        this.capture('./gamelikeapro.png');
-        this.echo("Done with game like a pro sweepstakes!");
+
+        casper.waitForText("Thank you for your submission", function() {
+            this.capture('./corsair-13.png');
+            this.echo("Done with gamecrate lucky 13 corsair giveaway!");
+        });
     });
 } else {
     casper.then(function () {
-        this.echo('skipping game like a pro sweepstakes');
+        this.echo('skipping gamecrate lucky 13 corsair giveaway');
     });
 }
-
-
 
 casper.run(function() {
     this.exit(returnCode);

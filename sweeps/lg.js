270cb8ba15e9893a19bf7f52958f7715f78ce8d8
Nathan Friedly <nathan@nfriedly.com>
2015-11-02 11:43:31 -0500

    lint-cleanup

--- sweeps/lg.js
+++ sweeps/lg.js
@@ -1,21 +1,21 @@
 var Twit = require('twit');
 var _ = require('lodash');
 var twit = new Twit({
-    consumer_key:         'xcg9KocD0mK0wgGWiiAUqAErK'
-    , consumer_secret:      'kyp7x7kKGoNRJmXXOFV1PRwEHCkVPD3zs9XtEmZ5SKVll0SNgm'
-    , access_token:         '34514563-isGDH2BwhULrkraMyWVcXMYznX2UnoMBkEOAbLaqJ'
-    , access_token_secret:  'Wtcdm5EdgsV9YPZogifq1GY5y3ASSrwXXgdGxixu9DhmG'
+    consumer_key:         'xcg9KocD0mK0wgGWiiAUqAErK',
+    consumer_secret:      'kyp7x7kKGoNRJmXXOFV1PRwEHCkVPD3zs9XtEmZ5SKVll0SNgm',
+    access_token:         '34514563-isGDH2BwhULrkraMyWVcXMYznX2UnoMBkEOAbLaqJ',
+    access_token_secret:  'Wtcdm5EdgsV9YPZogifq1GY5y3ASSrwXXgdGxixu9DhmG'
 });
 
 function post(msg) {
     return new Promise(function(resolve, reject) {
         twit.post('statuses/update', { status: msg }, function(err, data, response) {
             if (err) {
-                return reject(err)
+                return reject(err);
             }
             resolve('https://twitter.com/' + data.user.screen_name + '/status/' + data.id);
         });
-    })
+    });
 }
 
 var today = new Date();

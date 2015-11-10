4ca052b09cb04872dd0b42af3258dc23d89bb855
Nathan Friedly <nathan@nfriedly.com>
2015-11-10 10:08:59 -0500

    ramsey

---
+++ sweeps/ramsey.casper.js
@@ -0,0 +1,19 @@
+'use strict';
+var bot = require('../sweeps-bot.casper.js');
+var me = bot.me;
+
+bot.run({
+    name: 'Dave Ramseys Christmas',
+    end: 'December 21, 2015 11:59 pm CST',
+    startUrl: 'https://www.daveramsey.com/giveaway/christmas-giveaway-2015',
+    fn: function () {
+        this.fill('form.new_contest_entry', {
+            'contest_entry[first_name]': me.first,
+            'contest_entry[last_name]': me.last,
+            'contest_entry[email]': me.email2,
+            'contest_entry[phone]': me.phone,
+            'contest_entry[postal_code]': me.zip,
+        }, true);
+        this.waitForText('THANKS FOR ENTERING', bot.recordEntryConfirmed);
+    }
+});

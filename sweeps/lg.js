c5bd9d52a4ea38e8b252b4036438d879317c96ae
Nathan Friedly <nathan@nfriedly.com>
2015-11-20 10:53:48 -0500

    a little cleanup

--- sweeps/lg.js
+++ sweeps/lg.js
@@ -1,11 +1,5 @@
-var Twit = require('twit');
 var _ = require('lodash');
-var twit = new Twit({
-    consumer_key:         'xcg9KocD0mK0wgGWiiAUqAErK',
-    consumer_secret:      'kyp7x7kKGoNRJmXXOFV1PRwEHCkVPD3zs9XtEmZ5SKVll0SNgm',
-    access_token:         '34514563-isGDH2BwhULrkraMyWVcXMYznX2UnoMBkEOAbLaqJ',
-    access_token_secret:  'Wtcdm5EdgsV9YPZogifq1GY5y3ASSrwXXgdGxixu9DhmG'
-});
+var twit = require('../twitter.js');
 
 function post(msg) {
     return new Promise(function(resolve, reject) {
---
+++ sweeps/twitter-cleanup.js
@@ -0,0 +1,31 @@
+/**
+ * Created by nfriedly on 11/20/15.
+ */
+var twit = require('../twitter.js');
+
+console.log('cleaning up twitter...');
+twit.get('statuses/user_timeline', {screen_name:'misticflame', count: 50}, function(err, data, response) {
+    if (err) {
+        console.log('error fetching tweets', err);
+        return;
+    }
+    console.log('got %s recent tweets', data.length);
+    var numdeleted = 0;
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    data.forEach(function(tweet) {
+        if (
+            tweet.text.match(/@SYWSweeps/i) ||
+            (tweet.text.match(/@androidauth|@tabtimes|@realsoundguys/i) && new Date(tweet.created_at) < yesterday)
+        ) {
+            twit.post('statuses/destroy/' + tweet.id_str, function(err) {
+                if (err) {
+                    console.log('error deleting tweet %s %s \n%s', tweet.id_str, tweet.text, err, JSON.stringify(tweet, null, 2));
+                } else {
+                    numdeleted++;
+                }
+            });
+        }
+    });
+    console.log('deleted %s tweets', numdeleted);
+});

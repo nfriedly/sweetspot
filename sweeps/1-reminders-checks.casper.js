781c621e8e86c8c5ce9237eb8e84cc67c19ea153
Nathan Friedly <nathan@nfriedly.com>
2015-12-12 08:43:20 -0500

    starwars

--- sweeps/1-reminders-checks.casper.js
+++ sweeps/1-reminders-checks.casper.js
@@ -9,18 +9,6 @@ bot.run({
     noScreenshot: true,
     fn: function() {
         this.then(function () {
-            // these don't really belong here but meh...
-            this.echo('captcha\'d sweepstakes:');
-            [
-                {url: 'https://www.clorox.com/disney-pixar-inside-out/', end: 'December 15, 2015 11:59 pm EST'},
-                {url: 'http://www.soundguys.com/sound-guys-monthly-november-2015-7363/', end: 'November 28, 2015 11:59 pm EST'},
-                {url: 'http://tabtimes.com/tabtimes-monthly-november-2015-sony-xperia-z4-tablet-giveaway-34462/', end: 'November 27, 2015 11:59 pm EST'},
-                {url: 'http://www.androidauthority.com/blu-pure-xl-international-giveaway-3-phones-655587/', end: 'November 21, 2015 11:59 pm EST'},
-            ].forEach(function (s) {
-                    if (today < new Date(s.end)) {
-                        this.echo(s.url);
-                    }
-                }, this);
 
             this.echo('checking https://m.facebook.com/Newegg for new sweepstakes...');
             var text = this.fetchText('body');
